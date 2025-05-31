ce998d4a95f08787300ca55e839ce74657c152a5
pdh0128 <pdh0128a@gmail.com>
2025-05-31 12:58:31 +0900

    auth 디렉토리 구조 변경

--- src/main/java/com/example/auth/domain/auth/service/exception/NotFoundRefreshTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.example.auth.domain.auth.service.exception;
-
-public class NotFoundRefreshTokenException extends RuntimeException {
-  public NotFoundRefreshTokenException(String s) {
-    super(s);
-  }
-}
